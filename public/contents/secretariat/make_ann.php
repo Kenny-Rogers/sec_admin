23028c36ff0714c08509f5a294ef947c0ff52e93
Kenny-Rogers <ylodonu@gmail.com>
2018-02-08 20:56:58 +0000

    changing ui of announcement form

--- public/contents/secretariat/make_ann.php
+++ public/contents/secretariat/make_ann.php
@@ -2,31 +2,24 @@
     <div class="col-sm-offset-2 col-sm-8">
         <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Quick Example</h3>
+              <h3 class="box-title">Make Announcement</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <form role="form" enctype="multipart/form-data" method="post" action="http://127.0.0.1/final_proj_api/public/make_announcement.php">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
+                  <label for="exampleInputEmail1">Title</label>
                   <input class="form-control" name="title" id="exampleInputEmail1" placeholder="Enter email" type="text">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input class="form-control" name="message" id="exampleInputPassword1" placeholder="Password" type="text">
+                  <label for="exampleInputPassword1">Message</label>
+                  <textarea class="form-control" name="message"></textarea>
                 </div>
                 <input type="hidden" name="author" value="1" >
                 <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
+                  <label for="exampleInputFile">Image Upload</label>
                   <input id="exampleInputFile" name="image" type="file">
-
-                  <p class="help-block">Example block-level help text here.</p>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
                 </div>
               </div>
               <!-- /.box-body -->
